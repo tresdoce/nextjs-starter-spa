d48c64acbaf1b88f4cb6a5188d6225e1f9d06eb9
Maximiliano Delgado <mdelgado@baufest.com>
2021-03-20 00:55:01 -0300

    chore: add configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Config, Typings } from '@tresdoce-toolkit/core';
-import { setCookie } from '@utils';
 import setDistributionChannel from '@config/setDistributionChannel';
 import Configuration from '@config';
 import AppRouter from '../commons/AppRouter';
@@ -8,7 +7,6 @@ import AppRouter from '../commons/AppRouter';
 interface IndexProps {
   config: Typings.Config;
   generalError?: any;
-  session_id?: string;
 }
 
 // It's important to add the configuration object before any Config.getAppConfig call
@@ -18,21 +16,11 @@ class Index extends React.Component<IndexProps> {
   static displayName = 'Index';
 
   static async getInitialProps() {
-    if (process.env.SESSION_ID) {
-      const session_id = process.env.SESSION_ID;
-      return { ...Config.getAppConfig(), session_id };
-    }
-
     return Config.getAppConfig();
   }
 
   render() {
     setDistributionChannel();
-
-    if (this.props.session_id) {
-      setCookie('session_id', this.props.session_id);
-    }
-
     return <AppRouter {...this.props} />;
   }
 }
